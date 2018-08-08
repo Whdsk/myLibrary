e6230f81fc957bc0c9b3fe26f7458f07ef62c699
hangbo <1850154611@qq.com>
2018-08-08 14:30:57 +0800

    aaaa

---
+++ MyApplication/jartolibrary/src/main/java/com/example/jartolibrary/DensityUtils.java
@@ -0,0 +1,49 @@
+package com.example.jartolibrary;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+/**
+ * author gaohangbo
+ * date: 2018/8/6 0006.
+ */
+public class DensityUtils {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 获取当前页面的宽度
+     */
+    public static int getScreenWidth(@Nullable Context context) {
+        if (context != null) {
+            return context.getResources().getDisplayMetrics().widthPixels;
+        }
+        return 0;
+    }
+
+    /**
+     * 当前页面的高度
+     * 需要传递Activity的context才能保证准确
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(@Nullable Context context) {
+        if (context != null) {
+            return context.getResources().getDisplayMetrics().heightPixels;
+        }
+        return 0;
+    }
+}
